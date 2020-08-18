4c28381a2858f6b87afc2837b0c4cf6965823798
juanwmedia <juan@wmedia.es>
2020-08-18 21:19:27 +0200

    VueTalk header

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,19 @@
 <template>
   <div id="app">
+    <NavBarComponent />
     <router-view />
   </div>
 </template>
+
+<script>
+import NavBarComponent from "./components/NavBarComponent";
+
+export default {
+  name: "App",
+  components: {
+    NavBarComponent
+  }
+};
+</script>
 
 <style lang="scss"></style>
---
+++ src/components/NavBarComponent.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="container">
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <a class="navbar-item">
+          <img class="navbar__logo" src="../assets/img/VueTalk-logo.png" />
+        </a>
+        <a
+          @click.prevent="toggleNavBar"
+          role="button"
+          class="navbar-burger burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navBar"
+          ref="burger"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div id="navBar" class="navbar-menu" ref="navBar">
+        <div class="navbar-start">
+          <router-link class="navbar-item" to="/">
+            Home
+          </router-link>
+
+          <router-link class="navbar-item" to="/">
+            Create Room
+          </router-link>
+        </div>
+
+        <div class="navbar-end">
+          <div class="navbar-item">
+            <div class="buttons">
+              <router-link class="button is-primary" to="/">
+                <strong>Profile</strong>
+              </router-link>
+              <a @click="doLogout" class="button is-light">
+                Log out
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavBarComponent",
+  mounted() {
+    this.burger = this.$refs.burger;
+    this.navBar = this.$refs.navBar;
+  },
+  data() {
+    return {
+      burger: null,
+      navBar: null
+    };
+  },
+  methods: {
+    toggleNavBar() {
+      this.burger.classList.toggle("is-active");
+      this.navBar.classList.toggle("is-active");
+    },
+    doLogout() {}
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.navbar-item img {
+  max-height: none;
+}
+.navbar__logo {
+  width: 200px;
+  height: auto;
+}
+</style>
