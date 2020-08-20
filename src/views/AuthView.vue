c4d8b70f40e0d85ef76edc686ce64781280c0e51
juanwmedia <juan@wmedia.es>
2020-08-20 12:12:20 +0200

    Auth view

---
+++ src/views/AuthView.vue
@@ -0,0 +1,133 @@
+<template>
+  <article class="section">
+    <div class="container">
+      <div class="columns">
+        <div class="column is-half is-offset-one-quarter">
+          <!-- Login form -->
+          <template v-if="isLogin">
+            <h1 class="title has-text-centered">Login</h1>
+            <form @submit.prevent="doLogin">
+              <div class="field">
+                <label class="label">Email</label>
+                <div class="control">
+                  <input
+                    v-model="userData.email"
+                    class="input"
+                    type="email"
+                    placeholder="e.g. alexsmith@gmail.com"
+                    required
+                  />
+                </div>
+              </div>
+
+              <div class="field">
+                <label class="label">Password</label>
+                <div class="control">
+                  <input
+                    v-model="userData.password"
+                    class="input"
+                    type="password"
+                    required
+                  />
+                </div>
+              </div>
+
+              <div class="field has-text-right">
+                <div class="control">
+                  <button
+                    type="submit"
+                    class="button is-link"
+                    :class="{ 'is-loading': isLoading }"
+                  >
+                    Login
+                  </button>
+                </div>
+              </div>
+              <a href="#" @click="isLogin = false">Don't have an account?</a>
+            </form>
+          </template>
+          <!-- End of login form -->
+
+          <!-- Register form -->
+          <template v-else>
+            <h1 class="title has-text-centered">Register</h1>
+            <form @submit.prevent="doRegister">
+              <div class="field">
+                <label class="label">Name</label>
+                <div class="control">
+                  <input
+                    v-model="userData.name"
+                    class="input"
+                    type="text"
+                    placeholder="e.g Alex Smith"
+                    required
+                  />
+                </div>
+              </div>
+
+              <div class="field">
+                <label class="label">Email</label>
+                <div class="control">
+                  <input
+                    v-model="userData.email"
+                    class="input"
+                    type="email"
+                    placeholder="e.g. alexsmith@gmail.com"
+                    required
+                  />
+                </div>
+              </div>
+
+              <div class="field">
+                <label class="label">Password</label>
+                <div class="control">
+                  <input
+                    v-model="userData.password"
+                    class="input"
+                    type="password"
+                    required
+                  />
+                </div>
+              </div>
+
+              <div class="field has-text-right">
+                <div class="control">
+                  <button
+                    type="submit"
+                    class="button is-link"
+                    :class="{ 'is-loading': isLoading }"
+                  >
+                    Register
+                  </button>
+                </div>
+              </div>
+              <a href="#" @click="isLogin = true">Want to login?</a>
+            </form>
+          </template>
+          <!-- End of regisgter form -->
+        </div>
+      </div>
+    </div>
+  </article>
+</template>
+
+<script>
+export default {
+  name: "AuthView",
+  data() {
+    return {
+      isLogin: true,
+      isLoading: false,
+      userData: {
+        name: "",
+        email: "",
+        password: ""
+      }
+    };
+  },
+  methods: {
+    doRegister() {},
+    doLogin() {}
+  }
+};
+</script>
