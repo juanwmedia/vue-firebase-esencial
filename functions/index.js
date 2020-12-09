22bd4c12cfa766e674dc8b4b4228e34a9f6205de
juanwmedia <juan@wmedia.es>
2020-12-09 11:19:42 +0100

    Connect Cloud Functions to Cloud Firestore

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,20 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
+const regex = /perro/gi;
+
+exports.replaceDogs = functions.firestore
+  .document("/rooms/{room}/messages/{message}")
+  .onCreate((snapshot, context) => {
+    const original = snapshot.data().message;
+    const replaced = original.replace(regex, "gato");
+
+    if (original === replaced) {
+      return null; // Infinite loop protection
+    }
+
+    return snapshot.ref.update({ message: replaced }); // Always return something
+  });
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
